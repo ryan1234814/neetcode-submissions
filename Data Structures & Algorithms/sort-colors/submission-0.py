09ac621b7cca5f2d38ecaa917264b46ed1ee63b3
ryan1234814 <140492275+ryan1234814@users.noreply.github.com>
2026-06-23 12:44:21 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortColors(self, arr: List[int]) -> None:
+        for i in range(len(arr)):
+            for j in range(i+1,len(arr)):
+                if arr[i]>arr[j]:
+                    temp=arr[i]
+                    arr[i]=arr[j]
+                    arr[j]=temp
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
